88c6636e5d141d6c61272c2a0bc39d7bad758678
Dralexgon <99817451+Dralexgon@users.noreply.github.com>
2024-11-14 10:20:47 +0100

    tree

--- init.lua
+++ init.lua
@@ -946,6 +946,9 @@ require('lazy').setup({
   {
     'github/copilot.vim',
   },
+  {
+    'nvim-tree/nvim-tree.lua',
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
@@ -970,3 +973,30 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+-- Added by Alex
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require('nvim-tree').setup()
+
+-- OR setup with some options
+require('nvim-tree').setup {
+  sort = {
+    sorter = 'case_sensitive',
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+}
